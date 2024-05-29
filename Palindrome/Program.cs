fde85a930d9b884cf8a76160eea1f783770b2e11
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-29 11:53:49 +0100

    Ex 1, add exception and condition for less than 2 characters

---
+++ Palindrome/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public bool Palindrome(string str)
+        {
+            if (str == null)
+                //If the string is null
+                throw new ArgumentNullException("Invalid string"); 
+            else if (str.Length < 2) return true; //If string has less than 2 characters
+            return false;
+        }
+    }
+}
