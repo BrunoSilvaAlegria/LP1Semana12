4cc7a3872f38f764c516170de4e2703820aadbaf
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-30 14:31:05 +0100

    Ex 1, add IsPolindorme function in method

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Palindrome
 {
@@ -8,13 +9,35 @@ namespace Palindrome
         {
             Console.WriteLine("Hello, World!");
         }
-        public bool Palindrome(string str)
+        public static bool Palindrome(string str)
         {
             if (str == null)
                 //If the string is null
                 throw new ArgumentNullException("Invalid string"); 
+           
             else if (str.Length < 2) return true; //If string has less than 2 characters
-            return false;
+            
+            bool IsPalindrome(string s)
+            {
+                Queue<char> s1 = new Queue<char>(); //LIFO storage
+                Stack<char> s2 = new Stack<char>(); //FIFO storage
+
+                foreach (char c in s)
+                {
+                    //Puts each character in a queue and in a stack
+                    s1.Enqueue(c);
+                    s2.Push(c);
+                }
+                while (s1.Count > 0)
+                {
+                    //Gets the first character in the queue and in the stack 
+                    //and compares them
+                    if (s1.Dequeue() != s2.Pop()) return false; //If they're different
+                }
+                return true; //If they're the same
+            }
+            return IsPalindrome(str);
+            
         }
     }
 }
