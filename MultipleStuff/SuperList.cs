0e6de407a6b15fcea395ecb18d35db83919329fb
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-05-30 15:08:58 +0100

    Ex 5, add comments and complete GetMinMax2

--- MultipleStuff/SuperList.cs
+++ MultipleStuff/SuperList.cs
@@ -7,23 +7,37 @@ namespace MultipleStuff
 {
     public class SuperList : List<double>
     {
-        public SuperList() : base() {}
+        public SuperList() : base() {} //Inherits the double's list
 
         public void GetMinMax1(out double min, out double max)
         {
-            min = this[0];
-            max = this[0];
+            //They are the same for comparison purposes
+            min = this[0]; 
+            max = this[0]; 
 
             foreach(double n in this)
             {
-                if (n < min) min = n;
+                if (n < min) min = n; //Gets the minimum value
 
-                else if (n > max) max = n;
+                else if (n > max) max = n; //Gets the maximum value
             }
         }
         public void GetMinMax2()
         {
-            
+            MinMax minmax = new MinMax();
+
+           //They are the same for comparison purposes
+            double min = this[0]; 
+            double max = this[0];
+
+            foreach(double n in this)
+            {
+                if (n < min) minmax.Min = n;
+
+                else if (n > max) minmax.Max = n;
+            }
+
+            Console.WriteLine($"Min: {minmax.Min} | Max: {minmax.Max}");
         }
         public void GetMinMax3()
         {
@@ -32,6 +46,12 @@ namespace MultipleStuff
         public void GetMinMax4()
         {
             
+        }
+
+        public struct MinMax 
+        {
+            public double Min;
+            public double Max;
         }
     }
 }
